0c6b12d977e2886aea59443f587dce6778e54c75
hanaarena <hanaarena@gmail.com>
2025-09-10 17:47:49 +0900

    feat(client/news): add pagination for update news

--- client/src/components/UpdateNews.tsx
+++ client/src/components/UpdateNews.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { get } from "@lanz/utils";
 import { groupByYears } from "@/utils/times";
 import { Gradients } from "@/utils/const";
-import { Button, Group, HoverCard } from "@mantine/core";
+import { Button, Group, HoverCard, Pagination, Text } from "@mantine/core";
 
 type UpdateItem = {
   gid: string;
@@ -17,20 +17,36 @@ type UpdateItem = {
 };
 
 const apiEndpoint = import.meta.env.VITE_STEAM_API;
+const PageSize = 20;
+const now = Math.floor(Date.now() / 1000);
 
 export default function UpdateNews({ id }: { id: number | string }) {
   const [list, setList] = useState<Map<string, UpdateItem[]>>();
   const [count, setCount] = useState(0);
   const [filter, setFilter] = useState("");
+  const [enddate, setEnddate] = useState(now);
+  const [page, setPage] = useState(1);
+  const tempList = useRef<UpdateItem[]>([]);
 
   const handleUpdateFilter = (year: string) => {
     setFilter(year);
   };
 
+  const handlePageChange = (p: number) => {
+    let _enddate = now;
+    if (p > page) {
+      _enddate = (tempList.current.pop()?.date || now) - 1;
+    } else if (p < page) {
+      _enddate = (tempList.current.shift()?.date || now) + 1;
+    }
+    setPage(p);
+    setEnddate(_enddate);
+  };
+
   useEffect(() => {
     const getUpdateNews = async () => {
       const target = encodeURIComponent(
-        `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=${id}&count=20&format=json&maxlength=300&feeds=steam_community_announcements`
+        `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2?appid=${id}&count=${PageSize}&format=json&maxlength=300&feeds=steam_community_announcements&enddate=${enddate}`
       );
       const res = await get<{
         appnews: { newsitems: UpdateItem[]; count: number };
@@ -39,6 +55,7 @@ export default function UpdateNews({ id }: { id: number | string }) {
       if (res.appnews?.newsitems?.length) {
         // sort newest -> oldest
         const sorted = res.appnews.newsitems.sort((a, b) => b.date - a.date);
+        tempList.current = sorted;
         const grouped = groupByYears(sorted, "date");
         setList(grouped);
         const pickFirstYear = Array.from(grouped.keys())[0];
@@ -51,7 +68,7 @@ export default function UpdateNews({ id }: { id: number | string }) {
     };
 
     getUpdateNews();
-  }, [id]);
+  }, [id, enddate]);
 
   return (
     <div className="mb-4">
@@ -59,8 +76,7 @@ export default function UpdateNews({ id }: { id: number | string }) {
         <h1 className="text-2xl font-extrabold">Updates News</h1>
         <p className="text-xs">Total news: {count}</p>
       </header>
-
-      <div className="flex gap-2 flex-col">
+      <div className="flex gap-2 flex-col mb-3">
         <div>
           Years: &nbsp;
           {(list ? Array.from(list?.keys()) : []).map((year) => (
@@ -77,16 +93,16 @@ export default function UpdateNews({ id }: { id: number | string }) {
             </Button>
           ))}
         </div>
-        <div className="grid grid-rows-2 grid-flow-col auto-cols-max overflow-x-auto gap-2 whitespace-nowrap border border-gray-100">
+        <div className="grid grid-rows-2 grid-flow-col auto-fill-200 overflow-x-auto gap-2 border border-gray-100">
           {filter &&
             list?.get(filter)?.map((item, idx) => {
               const g = Gradients[idx % Gradients.length];
               return (
-                <Group justify="center" key={item.gid}>
+                <Group key={item.gid} className="grid">
                   <HoverCard width={280} shadow="md">
                     <HoverCard.Target>
                       <div
-                        className={`min-w-[180px] max-w-xs p-3 bg-gradient-to-br ${g} rounded-sm shadow cursor-pointer flex flex-col text-sm text-white`}
+                        className={`w-[200px] p-3 bg-gradient-to-br ${g} rounded-sm shadow cursor-pointer flex flex-col text-sm text-white`}
                       >
                         <a
                           className="font-medium truncate"
@@ -130,6 +146,18 @@ export default function UpdateNews({ id }: { id: number | string }) {
               );
             })}
         </div>
+      </div>
+      <div className="flex">
+        <Text className="mr-2!">
+          Showing {page} of {Math.ceil(count / PageSize)}
+        </Text>
+        <Pagination
+          total={Math.ceil(count / PageSize)}
+          size="sm"
+          radius="lg"
+          onChange={handlePageChange}
+          withPages={false}
+        />
       </div>
     </div>
   );
